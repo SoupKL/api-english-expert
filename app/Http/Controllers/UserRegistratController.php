7c7ba0a1c5c60f9012bc78ec7b032adb47466e6e
valia <valia0119@outlook.com>
2025-05-02 15:00:46 +1000

    регистрация тест

---
+++ app/Http/Controllers/UserRegistratController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserRegistratController extends Controller {
+
+	public function create() {
+
+	}
+	public function store(Request $request) {
+		$validator = Validator::make($request->all(), [
+			'name' => 'required|string|max:255',
+			'email' => 'required|string|email|max:255|unique:users',
+			'password' => 'required|string|min:8|confirmed',
+		]);
+
+		if ($validator->fails()) {
+			return response()->json($validator->errors(), 422);
+		}
+
+		$user = User::create([
+			'name' => $request->name,
+			'email' => $request->email,
+			'password' => Hash::make($request->password),
+		]);
+
+		return response()->json(['message' => 'User registered successfully', 'user' => $user], 201);
+	}
+}
